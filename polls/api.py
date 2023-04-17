c4f5ae6b384c7cdbf7a44657a310facb574f8af1
Nicolas Grunwald <nicolas.grunwald@semaphor.dk>
2023-04-17 14:46:07 +0200

    Added API endpoint to return either all or only approved questions

--- polls/api.py
+++ polls/api.py
@@ -1,8 +1,18 @@
 from ninja import Router
 from .models import Question
 
+from django.http import JsonResponse
+import json
+
 router = Router()
 
 @router.get('/')
 def say_hello(request):
     return f"Hello"
+
+@router.get('/questions')
+def get_all(request, only_approved: bool = "false"):
+    approved_question_list = Question.objects.filter(approved=True).values() if only_approved else Question.objects.values()
+    all_questions = []
+    for quest in approved_question_list: all_questions.append(quest)
+    return all_questions
