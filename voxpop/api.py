4bbfec47691a6eaa407e8bf24cf1dd411453c89e
Nicolas Grunwald <nicolas.grunwald@semaphor.dk>
2023-04-19 15:03:39 +0200

    Updated API

--- voxpop/api.py
+++ voxpop/api.py
@@ -5,30 +5,7 @@ import json
 
 router = Router()
 
+
 @router.get("/questions")
-def get_questions(request, only_approved: bool = "false"):
-    return (
-        list(q.as_dict() for q in Question.objects.filter(approved=True).all()) 
-        if only_approved else 
-        list(q.as_dict() for q in Question.objects.all())
-    )
-
-
-class VoteSchema(Schema):
-    name: str
-    question_id: int
-
-class Error(Schema):
-    msg: str
-
-"""
-@router.post("/vote", response={200: VoteSchema, 405: Error})
-def vote_on_question(request, data: VoteSchema):
-    if Vote.objects.all().filter(vote_by=data.name).first() == None:
-        v = Vote(
-            vote_by=data.name, 
-            question=Question.objects.get(pk=data.question_id)
-        )
-        v.save()
-        return 200, {"message": "Success"}
-    else: return 405, {"message": "Already posted on question"}"""
+def get_questions(request):
+    return list(q.as_dict() for q in Question.objects.filter(approved=True).all())
