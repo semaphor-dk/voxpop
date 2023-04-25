65486dc719dd809ef86155d698c1929ef97ca198
Nicolas Grunwald <nicolas.grunwald@semaphor.dk>
2023-04-25 15:57:13 +0200

    Expanded REST API with mild error handling

--- voxpop/api.py
+++ voxpop/api.py
@@ -39,17 +39,43 @@ class VoteSchema(Schema):
     question_id: int
     created_by: str
 
-
-@router.get("/questions", response=list[QuestionSchema])
-def questions(request):
-    return list(get_questions(state=Question.State.APPROVED))
+class Error(Schema):
+    msg: str
 
 
-@router.get("/voxpops", response=list[VoxpopSchema])
+@router.get("/", response=list[VoxpopSchema])
 def voxpops(request):
     return list(get_voxpops())
 
 
-@router.get("/votes", response=list[VoteSchema])
-def votes(request):
-    return list(get_votes())
+@router.get("/{voxpop_id}", response=VoxpopSchema)
+def voxpop(request, voxpop_id: int):
+    return get_voxpops(voxpop_id=voxpop_id)
+
+
+@router.get("/{voxpop_id}/questions", response=list[QuestionSchema])
+def questions(request, voxpop_id: int):
+    # TODO: Make sure voxpop exists.
+    return list(get_questions(state=Question.State.APPROVED).filter(voxpop_id=voxpop_id))
+
+
+@router.get("/{voxpop_id}/questions/{question_id}", response={200: QuestionSchema, 500: Error})
+def question(request, voxpop_id: int, question_id: int):
+    if get_questions(question_id=question_id).voxpop_id == voxpop_id:
+        return 200, get_questions(state=Question.State.APPROVED, question_id=question_id)
+    return 500, {"msg": "Container error"}
+
+
+@router.get("/{voxpop_id}/questions/{question_id}/votes", response={200: list[VoteSchema], 500: Error})
+def votes(request, voxpop_id: int, question_id: int):
+    if get_questions(question_id=question_id).voxpop_id == voxpop_id:
+        return list(get_votes().filter(question_id=question_id))
+    return 500, {"msg": "Container error"}
+
+
+@router.get("/{voxpop_id}/questions/{question_id}/votes/{vote_id}", response={200: VoteSchema, 500: Error})
+def vote(request, voxpop_id: int, question_id: int, vote_id: int):
+    if (get_questions(question_id=question_id).voxpop_id == voxpop_id and 
+        get_votes(vote_id=vote_id).question_id == question_id):
+        return get_votes(vote_id=vote_id)
+    return 500, {"msg": "Container error"}
