1735d639a8b00e49b6ff67c521556085cf038257
Bo Frederiksen <bo.frederiksen@semaphor.dk>
2023-05-03 14:43:02 +0200

    Added deployment script.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_NAME=${PWD##*/}
+ZIP_FILE="/tmp/$PROJECT_NAME.zip"
+DEPLOYMENT_HOST="klapperslange1.semaphor.dk"
+DEPLOYMENT_FOLDER="/opt/$PROJECT_NAME/$PROJECT_NAME-main"
+SERVICE_NAME="$PROJECT_NAME.service"
+
+git archive --format=zip --output $ZIP_FILE HEAD
+scp $ZIP_FILE $DEPLOYMENT_HOST:/tmp/
+rm $ZIP_FILE
+ssh $DEPLOYMENT_HOST 'sudo unzip -o' $ZIP_FILE ' -d' $DEPLOYMENT_FOLDER
+ssh $DEPLOYMENT_HOST 'rm' $ZIP_FILE
+
+ssh $DEPLOYMENT_HOST 'sudo systemctl restart' $SERVICE_NAME
