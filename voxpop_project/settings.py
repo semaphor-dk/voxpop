20a0c84efd4a02d249eb7404223104d38d488ad5
Bo Frederiksen <bo.frederiksen@semaphor.dk>
2023-05-04 17:59:29 +0200

    Work now!

--- voxpop_project/settings.py
+++ voxpop_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-p&u=b2dtf%i%g)e$&&=lx!ojns0e0f^kd*#v5!!^gcr*vvb+^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['voxpop.semaphor.dk', 'localhost', '127.0.0.1']
 
 
 # Application definition
