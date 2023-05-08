cf715eda1f24e1aa66732f304f6e02461f603bae
Bo Frederiksen <bo.frederiksen@semaphor.dk>
2023-05-08 17:12:52 +0200

    Removed duplicate static folder.

--- voxpop_project/settings.py
+++ voxpop_project/settings.py
@@ -97,9 +97,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [
-    BASE_DIR / "voxpop/static"
-]
+# STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
