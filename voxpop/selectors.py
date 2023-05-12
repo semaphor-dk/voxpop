3c74de20f51bcca49e08fd933f6915298a5de318
Nicolas Grunwald <nicolas.grunwald@semaphor.dk>
2023-05-12 13:49:48 +0200

    Refactored imports and added is_active annotation to voxpop selector

--- voxpop/selectors.py
+++ voxpop/selectors.py
@@ -1,14 +1,15 @@
 from __future__ import annotations  # For Python3.9 users.
 
 from uuid import UUID
+from django.utils import timezone
 
-from django.db.models import Count
-from django.db.models import QuerySet
+from django.db.models import (
+    Count, Case, When, BooleanField, Q, QuerySet
+)
 
-from voxpop.models import Question
-from voxpop.models import Vote
-from voxpop.models import Voxpop
-from voxpop.models import Organisation
+from voxpop.models import (
+    Question, Vote, Voxpop, Organisation
+)
 
 
 def get_organisations(
@@ -59,8 +60,14 @@ def get_voxpops(
     organisation_id: UUID | None = None,
     ) -> QuerySet[Voxpop] | Voxpop:
     
+    current_time = timezone.now()
     voxpops = Voxpop.objects.all().annotate(
         question_count=Count("question", distinct=True),
+        is_active=Case(
+            When(Q(starts_at__lte=current_time) & Q(expires_at__gte=current_time), then=True),
+            default=False,
+            output_field=BooleanField(),
+        )
     )
 
     if organisation_id:
