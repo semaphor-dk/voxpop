e985c0378446b9c36bb178198d9b450e2602753b
Bo Frederiksen <bo.frederiksen@semaphor.dk>
2023-08-28 17:17:56 +0200

    Handle frontend translations and submit new questions.

--- voxpop/static/voxpop.js
+++ voxpop/static/voxpop.js
@@ -2,7 +2,8 @@
 /* jshint -W100 */
 (function() {
 	'use strict';
-	var allSse = [];
+	let allSse = [];
+	let translations = {};
 	let voxpopElements = document.querySelectorAll('*[data-voxpop-uuid*="-"]');
 	voxpopElements.forEach(function (voxpopElm) {
 		let hostPort = (voxpopElm.dataset.voxpopHost) ? `//${ voxpopElm.dataset.voxpopHost }` : '';
@@ -18,8 +19,7 @@
 		};
 		sse.addEventListener("new_question", function (evt) {
 			let data = JSON.parse(evt.data);
-			console.log(data);
-			voxpopElm.insertAdjacentHTML("afterbegin", createHTMLforQuestion(data));
+			voxpopElm.insertAdjacentHTML("afterbegin", createHTMLforQuestion(data, translations));
 		});
 		sse.addEventListener("new_vote", function (evt) {
 			let data = JSON.parse(evt.data);
@@ -35,67 +35,100 @@
 		voxpopElm.className = ["connecting", "live", "disconnected"][state];
 	}
 
-	function createHTMLforQuestion(q) {
-		return `<div data-voxpop-question-uuid="${ q.uuid }">
-	<blockquote>${ q.text }</blockquote>
-	<div class="displayName">${ q.display_name }</div>
-	<div class="votes"><span>${ q.vote_count || 0 }</span> votes</div>
-	<button type="button" class="vote">Vote</button>
+	function getLanguage(voxpopElm) {
+			let langElm = voxpopElm.closest('[lang]');
+			return (langElm) ? langElm.getAttribute('lang') : 'en';
+	}
+
+	function getJSON(url) {
+		return new Promise(function (resolve, reject) {
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+			xhr.open('get', url, true);
+			xhr.onload = function () {
+				if (this.status >= 200 && this.status < 300) {
+					try {
+						resolve(JSON.parse(xhr.response));
+					}
+					catch(error) {
+						reject(error);
+					}
+				} else {
+					reject({
+						status: this.status,
+						message: xhr.statusText
+					});
+				}
+			};
+			xhr.onerror = function () {
+				reject({
+					status: this.status,
+					message: xhr.statusText
+				});
+			};
+			xhr.send();
+		});
+	}
+
+	function createHTMLforQuestion(question, translations) {
+		let voteCountLabel = (question.vote_count || 0)  === 1 ? 'VoteCounterLabelOne' : 'VoteCounterLabel';
+		return `<div data-voxpop-question-uuid="${ question.uuid }">
+	<blockquote>${ question.text }</blockquote>
+	<div class="displayName">${ question.display_name }</div>
+	<div class="votes"><span>${ question.vote_count || 0 }</span> ${ translations[voteCountLabel] }</div>
+	<button type="button" class="vote">${ translations['VoteButton'] }</button>
 </div>\n`;
 	}
 
-	function createNewQuestionForm(hostname, voxpopUuid) {
+	function createNewQuestionForm(hostname, voxpopUuid, translations) {
 		return `<form action="${ hostname }/api/voxpops/${ voxpopUuid }/questions/new" method="POST">
     <input type="hidden" name="csrfmiddlewaretoken" value="CF7wx3OUxgmnjF4KWO0FJsQcrjJIk0luPQDtv0XBA6UFi42MwbT4yoav3cBmxcPW">
     <textarea name="text" rows="3" maxlength="150" autofocus required></textarea>
-    - <input name="display_name" type="text" maxlength="50" placeholder="Dit navn">
-    <button type="submit" class="primary">Send</button>
+    - <input name="display_name" type="text" maxlength="50" placeholder="${ translations['NamePlaceholder'] }">
+    <button type="submit" class="primary">${ translations['SubmitQuestionButton'] }</button>
 </form>`;
 	}
 
 	function renderVoxpop(voxpopElm, questionsUrl) {
-		let xhr = new XMLHttpRequest();
-		xhr.open("get", questionsUrl, true);
-		xhr.withCredentials = true;
-		xhr.onload = function () {
-			if (this.status >= 200 && this.status < 300) {
-				let questions = JSON.parse(xhr.response);
-				let fragment = "\n";
-				questions.approved.forEach(function (question) {
-					fragment += createHTMLforQuestion(question);
+		let lang = getLanguage(voxpopElm);
+		let promises = [
+			getJSON(questionsUrl),
+			getJSON(((voxpopElm.dataset.voxpopHost) ? '//' + voxpopElm.dataset.voxpopHost : '') + '/static/translations.json')
+		];
+		Promise.all(promises).then(function (values) {
+			let questions = values[0];
+			translations = values[1][lang];
+			if (typeof translations === 'undefined') translations = values[1]['en'];
+			let fragment = "\n";
+			questions.approved.forEach(function (question) {
+				fragment += createHTMLforQuestion(question, translations);
+			});
+			fragment += createNewQuestionForm((voxpopElm.dataset.voxpopHost) ? '//' + voxpopElm.dataset.voxpopHost : '', voxpopElm.dataset.voxpopUuid, translations);
+			voxpopElm.innerHTML = fragment;
+			voxpopElm.addEventListener('click', function (evt) {
+				if (evt.target.classList.contains('vote')) {
+					let questionUuid = evt.target.closest('div[data-voxpop-question-uuid]').dataset.voxpopQuestionUuid;
+					let voxpopData = evt.target.closest('*[data-voxpop-uuid').dataset;
+					let hostPort = (voxpopData.voxpopHost) ? `//${ voxpopData.voxpopHost }` : '';
+					vote(`${ hostPort }/api/voxpops/${ voxpopData.voxpopUuid }/questions/${ questionUuid }/vote`);
+				}
+			});
+			voxpopElm.querySelector('form').addEventListener('submit', function (evt) {
+				evt.preventDefault();
+				let formData = new FormData(evt.target);
+				let object = {};
+				formData.forEach(function(value, key){
+					object[key] = value;
 				});
-				fragment += createNewQuestionForm((voxpopElm.dataset.voxpopHost) ? '//' + voxpopElm.dataset.voxpopHost : '', voxpopElm.dataset.voxpopUuid);
-				voxpopElm.innerHTML = fragment;
-				voxpopElm.addEventListener('click', function (evt) {
-					if (evt.target.classList.contains('vote')) {
-						let questionUuid = evt.target.closest('div[data-voxpop-question-uuid]').dataset.voxpopQuestionUuid;
-						let voxpopData = evt.target.closest('*[data-voxpop-uuid').dataset;
-						let hostPort = (voxpopData.voxpopHost) ? `//${ voxpopData.voxpopHost }` : '';
-						vote(`${ hostPort }/api/voxpops/${ voxpopData.voxpopUuid }/questions/${ questionUuid }/vote`);
-					}
+				const xhr = new XMLHttpRequest();
+				xhr.withCredentials = true;
+				xhr.addEventListener('load', function () {
+					evt.target.querySelector('textarea[name="text"]').value = '';
 				});
-				voxpopElm.querySelector('form').addEventListener('submit', function (evt) {
-					evt.preventDefault();
-					let formData = new FormData(evt.target);
-					let object = {};
-					formData.forEach(function(value, key){
-						object[key] = value;
-					});
-					const xhr = new XMLHttpRequest();
-					xhr.withCredentials = true;
-					xhr.addEventListener('load', function () {
-						console.dir(xhr.response);
-					});
-					xhr.open(evt.target.method, evt.target.action);
-					xhr.send(JSON.stringify(object));
-				});
-			}
-		};
-		xhr.onerror = function (evt) {
-			console.log('XHR error:');
-			console.dir(evt.target);
-		};
-		xhr.send();
+				xhr.open(evt.target.method, evt.target.action);
+				xhr.send(JSON.stringify(object));
+			});
+		});
 	}
 
 	function vote(endpoint) {
